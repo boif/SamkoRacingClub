7194414eb0d203fcc5ad13c176cb5b7cf13b3bed
boif <chlentim7@gmail.com>
2022-10-25 15:41:33 +0300

    commit

--- SamkoRacingClub/urls.py
+++ SamkoRacingClub/urls.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
 from django.contrib import admin
 from SRC import views, forms
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.static import serve
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -27,6 +28,9 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
