7d6beb88ed6d2e34105a8a8f0719efb302087d07
boif <chlentim7@gmail.com>
2022-10-25 19:59:12 +0300

    commit

--- SamkoRacingClub/settings.py
+++ SamkoRacingClub/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'SamkoRacingClub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
