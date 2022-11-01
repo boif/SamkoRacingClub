f036e591f13a78fe0033c044d5af2040c65b912e
boif <chlentim7@gmail.com>
2022-11-01 19:41:39 +0300

    commit

--- SRC/models.py
+++ SRC/models.py
@@ -5,5 +5,6 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images', verbose_name='Image')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name = 'Price', default = 0)
 
+    objects = models.Manager()
     def __str__(self):
         return self.title
