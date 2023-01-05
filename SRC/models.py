f982077c26f83470f90f9d6c21ecbb5aa01b4846
boif <chlentim7@gmail.com>
2023-01-05 04:42:43 +0300

    commit

--- SRC/models.py
+++ SRC/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
     
 class Image(models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
     title = models.CharField(max_length=70, verbose_name='Name')
     description = models.CharField(max_length=300, null=True, blank=True, verbose_name='Description')
     image = models.ImageField(upload_to='images', verbose_name='Image')
@@ -12,7 +12,7 @@ class Image(models.Model):
         return self.title
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     profile_pic = models.ImageField(upload_to='images/profile', default='images\profile\default.png', blank=True)
     vk = models.CharField(max_length=50, null=True, blank=True)
 
